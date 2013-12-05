713480da85ba5586cd683ae252c43602f20e24ae
Jim Lieb <jlieb@panasas.com>
2013-12-05 15:07:32 -0800

    Add new config parse tree to runtime config struct infrastructure
    This is table driven semantic and validation logic.  The table is a
    mapping between the parse tree nodes and the structure members.
    
    * add enum, path and unique (only once) param processing
    
    * Do init pass for block before lookup.  Missing block fallback to defaults
    
    * Add attributes mode processing to config processor
    
    * Add sub-block and inet addr and port processing
    
    Signed-off-by: Jim Lieb <jlieb@panasas.com>

--- src/ConfigParsing/config_parsing.c
+++ src/ConfigParsing/config_parsing.c
@@ -20,16 +20,22 @@
  * ---------------------------------------
  */
 #include "config.h"
-#include "config_parsing.h"
-#include "analyse.h"
 #include <stdio.h>
 #include <errno.h>
 #include <assert.h>
 #if HAVE_STRING_H
 #include <string.h>
 #endif
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include "config_parsing.h"
+#include "analyse.h"
 #include "abstract_mem.h"
 #include "conf_yacc.h"
+#include "log.h"
+#include "fsal_convert.h"
 
 struct parser_state parser_state;
 
@@ -86,6 +92,667 @@ void config_Free(config_file_t config)
 	free_parse_tree((struct config_root *)config);
 	return;
 
+}
+
+static bool convert_bool(struct config_node *node,
+			 bool *b)
+{
+	if (!strcasecmp(node->u.varvalue, "1") ||
+	    !strcasecmp(node->u.varvalue, "yes") ||
+	    !strcasecmp(node->u.varvalue, "true")) {
+		*b = true;
+		return true;
+	}
+	if (!strcasecmp(node->u.varvalue, "0") ||
+	    !strcasecmp(node->u.varvalue, "no") ||
+	    !strcasecmp(node->u.varvalue, "false")) {
+		*b = false;
+		return true;
+	}
+	LogMajor(COMPONENT_CONFIG,
+		 "At (%s:%d): %s (%s) should be 'true' or 'false'",
+		 node->filename,
+		 node->linenumber,
+		 node->name,
+		 node->u.varvalue);
+	return false;
+}
+
+static bool convert_int(struct config_node *node,
+			int64_t min, int64_t max,
+			int64_t *num)
+{
+	int64_t val;
+	char *endptr;
+
+	errno = 0;
+	val = strtoll(node->u.varvalue, &endptr, 10);
+	if (*node->u.varvalue != '\0' && *endptr == '\0') {
+		if (errno != 0 || val < min || val > max) {
+			LogMajor(COMPONENT_CONFIG,
+				 "At (%s:%d): %s (%s) is out of range",
+				 node->filename,
+				 node->linenumber,
+				 node->name,
+				 node->u.varvalue);
+			return false;
+		}
+	} else {
+		LogMajor(COMPONENT_CONFIG,
+			 "At (%s:%d): %s (%s) is not an integer",
+			 node->filename,
+			 node->linenumber,
+			 node->name,
+			 node->u.varvalue);
+		return false;
+	}
+	*num = val;
+	return true;
+}
+
+static bool convert_uint(struct config_node *node,
+			 uint64_t min, uint64_t max,
+			 uint64_t *num)
+{
+	uint64_t val;
+	char *endptr;
+
+	errno = 0;
+	val = strtoull(node->u.varvalue, &endptr, 10);
+	if (*node->u.varvalue != '\0' && *endptr == '\0') {
+		if (errno != 0 || val < min || val > max) {
+			LogMajor(COMPONENT_CONFIG,
+				 "At (%s:%d): %s (%s) is out of range",
+				 node->filename,
+				 node->linenumber,
+				 node->name,
+				 node->u.varvalue);
+			return false;
+		}
+	} else {
+		LogMajor(COMPONENT_CONFIG,
+			 "At (%s:%d): %s (%s) is not an integer",
+			 node->filename,
+			 node->linenumber,
+			 node->name,
+			 node->u.varvalue);
+		return false;
+	}
+	*num = val;
+	return true;
+}
+
+/* scan a list of CSV tokens.  move to parser!
+ */
+
+static int convert_list(struct config_node *node,
+			struct config_item *item,
+			uint32_t *flags)
+{
+	struct config_item_list *tok;
+	char *csv_list = alloca(strlen(node->u.varvalue) + 1);
+	char *sp, *cp, *ep;
+	bool found;
+	int errors = 0;
+
+	*flags = 0;
+	strcpy(csv_list, node->u.varvalue);
+	sp = csv_list;
+	ep = sp + strlen(sp);
+	while (cp < ep) {
+		cp = index(sp, ',');
+		if (cp != NULL)
+			*cp++ = '\0';
+		else
+			cp = ep;
+		tok = item->u.lst.tokens;
+		found = false;
+		while (tok->token != NULL) {
+			if (strcasecmp(sp, tok->token) == 0) {
+				*flags |= tok->value;
+				found = true;
+			}
+			tok++;
+		}
+		if (!found) {
+			LogMajor(COMPONENT_CONFIG,
+				 "At (%s:%d): %s has unknown token (%s)",
+				 node->filename,
+				 node->linenumber,
+				 node->name,
+				 sp);
+			errors++;
+		}
+		sp = cp;
+	}
+	return errors;
+}
+
+static int convert_enum(struct config_node *node,
+			struct config_item *item,
+			uint32_t *val)
+{
+	struct config_item_list *tok;
+	bool found;
+	int errors = 0;
+
+	tok = item->u.lst.tokens;
+	found = false;
+	while (tok->token != NULL) {
+		if (strcasecmp(node->u.varvalue, tok->token) == 0) {
+			*val = tok->value;
+			found = true;
+		}
+		tok++;
+	}
+	if (!found) {
+		LogMajor(COMPONENT_CONFIG,
+			 "At (%s:%d): %s has unknown token (%s)",
+			 node->filename,
+			 node->linenumber,
+			 node->name,
+			 node->u.varvalue);
+		errors++;
+	}
+	return errors;
+}
+
+static int convert_inet_addr(struct config_node *node,
+			     struct config_item *item,
+			     int ai_family,
+			     struct sockaddr *sock)
+{
+	struct addrinfo hints;
+	struct addrinfo *res = NULL;
+	int rc;
+
+	hints.ai_flags = AI_ADDRCONFIG;
+	hints.ai_family = ai_family;
+	hints.ai_socktype = 0;
+	hints.ai_protocol = 0;
+	rc = getaddrinfo(node->u.varvalue, NULL,
+			 &hints, &res);
+	if (rc == 0) {
+		memcpy(sock, res->ai_addr, res->ai_addrlen);
+		if (res->ai_next != NULL)
+			LogInfo(COMPONENT_CONFIG,
+				"At (%s:%d): Multiple addresses for %s = %s",
+				node->filename,
+				node->linenumber,
+				node->name,
+				node->u.varvalue);
+	} else {
+		LogMajor(COMPONENT_CONFIG,
+			 "At (%s:%d): No IP address found for %s = %s because:%s",
+			 node->filename,
+			 node->linenumber,
+			 item->name, node->u.varvalue,
+			 gai_strerror(rc));
+	}
+	freeaddrinfo(res);
+	return rc;
+}
+
+static int do_block_init(struct config_item *params,
+			 void *param_struct);
+
+static int do_block_load(struct config_node *blk,
+			 struct config_item *params,
+			 void *param_struct);
+
+/**
+ * @brief Process a sub-block
+ *
+ * The item arg supplies two function pointers that
+ * are defined as follows:
+ *
+ * param_mem
+ *  This function manages memory for the sub-block's processing.
+ *  It has two arguments, a pointer to the parent param struct and
+ *  a pointer to the child param struct.
+ *  If the child argument is NULL, it returns a pointer to a usable
+ *  child param struct.  This can either be allocate memory or a pointer to
+ *  existing memory.
+ *
+ *  If the child argument is not NULL, it is the pointer it returned above.
+ *  The function reverts whatever it did above.
+ *
+ * attach
+ *  This function attaches the build param struct to its parent.
+ *  If the second argument is NULL, the function initializes
+ *  the parent's linkage.  For glists, this would be a glist_init().
+ *  It is called by both do_subblock for this block and by
+ *  do_block_init() for the parent.  This makes the parent whole
+ *  even if this block parse throws errors.
+ *
+ * Both of these functions are called in the context of the parent parse.
+ * In particular, the attach initialization is to the parent, not the child.
+ * The child does its own init of things like glist in param_mem.
+ *
+ * @param node - parse node of the subblock
+ * @param item - config_item describing block
+ * @param parent - pointer to the parent structure
+ *
+ * @ return 0 on success, non-zero on errors
+ */
+
+static int do_subblock(struct config_node *node,
+		       struct config_item *item,
+		       void *parent)
+{
+	void *param_struct;
+	int rc = 0;
+
+	if (node->type != TYPE_BLOCK) {
+		LogCrit(COMPONENT_CONFIG,
+			"At (%s:%d): %s is not a block!",
+			 node->filename,
+			 node->linenumber,
+			item->name);
+		return 1;
+	}
+	param_struct = item->u.blk.param_mem(parent, NULL);
+	if (param_struct == NULL) {
+		LogCrit(COMPONENT_CONFIG,
+			"At (%s:%d): Could not allocate subblock for %s",
+			 node->filename,
+			 node->linenumber,
+			item->name);
+		return 1;
+	}
+	rc = do_block_init(item->u.blk.sub_blk, param_struct);
+	if (rc != 0) {
+		LogCrit(COMPONENT_CONFIG,
+			"At (%s:%d): Could not initialize parameters for %s",
+			node->filename,
+			node->linenumber,
+			item->name);
+		item->u.blk.param_mem(parent, param_struct);
+		return 1;
+	}
+	rc = do_block_load(node, item->u.blk.sub_blk, param_struct);
+	if (rc != 0) {
+		LogCrit(COMPONENT_CONFIG,
+			"At (%s:%d): Could not process parameters for %s",
+			node->filename,
+			node->linenumber,
+			item->name);
+		item->u.blk.param_mem(parent, param_struct);
+		return 1;
+	}
+	item->u.blk.attach(parent, param_struct);
+	return 0;
+}
+
+/**
+ * @brief Lookup the first node in the list by this name
+ *
+ * @param list - head of the glist
+ * @param name - node name of interest
+ *
+ * @return first matching node or NULL
+ */
+
+static struct config_node *lookup_node(struct glist_head *list,
+				       const char *name)
+{
+	struct config_node *node;
+	struct glist_head *ns;
+	
+	glist_for_each(ns, list) {
+		node = glist_entry(ns, struct config_node, node);
+		if (strcasecmp(name, node->name) == 0) {
+			node->found = true;
+			return node;
+		}
+	}
+	return NULL;
+}
+
+/**
+ * @brief Lookup the next node in list
+ *
+ * @param list - head of the glist
+ * @param start - continue the lookup from here
+ * @param name - node name of interest
+ *
+ * @return first matching node or NULL
+ */
+
+
+static struct config_node *lookup_next_node(struct glist_head *list,
+				     struct glist_head *start,
+				     const char *name)
+{
+	struct config_node *node;
+	struct glist_head *ns;
+
+	glist_for_each_next(start, ns, list) {
+		node = glist_entry(ns, struct config_node, node);
+		if (strcasecmp(name, node->name) == 0) {
+			node->found = true;
+			return node;
+		}
+	}
+	return NULL;
+}
+
+static int do_block_init(struct config_item *params,
+			 void *param_struct)
+{
+	struct config_item *item;
+	caddr_t *param_addr;
+	struct sockaddr_in *sock;
+	struct sockaddr_in6 *sock6;
+	int rc;
+	int errors = 0;
+
+	for (item = params; item->name != NULL; item++) {
+		param_addr = (caddr_t *)((uint64_t)param_struct + item->off);
+		switch (item->type) {
+		case CONFIG_INT16:
+			*(int16_t *)param_addr = item->u.i16.def;
+			break;
+		case CONFIG_UINT16:
+			*(uint16_t *)param_addr = item->u.ui16.def;
+			break;
+		case CONFIG_INT32:
+			*(int32_t *)param_addr = item->u.i32.def;
+			break;
+		case CONFIG_UINT32:
+			*(uint32_t *)param_addr = item->u.ui32.def;
+			break;
+		case CONFIG_INT64:
+			*(int64_t *)param_addr = item->u.i64.def;
+			break;
+		case CONFIG_UINT64:
+			*(uint64_t *)param_addr = item->u.ui64.def;
+			break;
+		case CONFIG_STRING:
+		case CONFIG_PATH:
+			if (item->u.str.def)
+				*(char **)param_addr = gsh_strdup(item->u.str.def);
+			else
+				*(char **)param_addr = NULL;
+			break;
+		case CONFIG_BOOL:
+			*(bool *)param_addr = item->u.b.def;
+			break;
+		case CONFIG_LIST:
+		case CONFIG_ENUM:
+			*(uint32_t *)param_addr = item->u.lst.def;
+			break;
+		case CONFIG_IPV4_ADDR:
+			sock = (struct sockaddr_in *)param_addr;
+			memset(&sock->sin_addr, 0, sizeof(struct in_addr));
+			sock->sin_family = AF_INET;
+			rc = inet_pton(AF_INET,
+				       item->u.ipv4.def, &sock->sin_addr);
+			if (rc <= 0) {
+				LogWarn(COMPONENT_CONFIG,
+					"Cannot set IPv4 default for %s to %s",
+					item->name, item->u.ipv4.def);
+				errors++;
+			}
+			break;
+		case CONFIG_IPV6_ADDR:
+			sock6 = (struct sockaddr_in6 *)param_addr;
+			memset(&sock6->sin6_addr, 0, sizeof(struct in6_addr));
+			sock6->sin6_family = AF_INET6;
+			rc = inet_pton(AF_INET6,
+				       item->u.ipv6.def, &sock6->sin6_addr);
+			if (rc <= 0) {
+				LogWarn(COMPONENT_CONFIG,
+					"Cannot set IPv4 default for %s to %s",
+					item->name, item->u.ipv6.def);
+				errors++;
+			}
+			break;
+		case CONFIG_INET_PORT:
+			*(uint16_t *)param_addr = htons(item->u.ui16.def);
+			break;
+		case CONFIG_BLOCK:
+			item->u.blk.attach(param_addr, NULL);
+			break;
+		default:
+			LogCrit(COMPONENT_CONFIG,
+				"Cannot set default for parameter %s, type(%d) yet",
+				item->name, item->type);
+			errors++;
+			break;
+		}
+	}
+	return errors;
+}
+
+static int do_block_load(struct config_node *blk,
+			 struct config_item *params,
+			 void *param_struct)
+{
+	struct config_item *item;
+	caddr_t *param_addr;
+	struct sockaddr *sock;
+	struct config_node *node, *next_node = NULL;
+	struct glist_head *ns;
+	int rc;
+	int errors = 0;
+
+	for (item = params; item->name != NULL; item++) {
+		int64_t val;
+		uint64_t uval;
+		uint32_t flags;
+		bool bval;
+
+		node = lookup_node(&blk->u.sub_nodes, item->name);
+		while (node != NULL) {
+			next_node = lookup_next_node(&blk->u.sub_nodes,
+						     &node->node, item->name);
+			if (next_node != NULL &&
+			    (item->flags & CONFIG_UNIQUE)) {
+				LogMajor(COMPONENT_CONFIG,
+					 "At (%s:%d): Parameter %s set more than once",
+					 next_node->filename,
+					 next_node->linenumber,
+					 next_node->name);
+				errors++;
+				node = next_node;
+				continue;
+			}
+			param_addr = (caddr_t *)((uint64_t)param_struct + item->off);
+			switch (item->type) {
+			case CONFIG_INT16:
+				if (convert_int(node, item->u.i16.minval,
+						item->u.i16.maxval,
+						&val))
+					*(int16_t *)param_addr = (int16_t)val;
+				else
+					errors++;
+				break;
+			case CONFIG_UINT16:
+				if (convert_uint(node, item->u.ui16.minval,
+						 item->u.ui16.maxval,
+						 &uval))
+					*(uint16_t *)param_addr = (uint16_t)uval;
+				else
+					errors++;
+				break;
+			case CONFIG_INT32:
+				if (convert_int(node, item->u.i32.minval,
+						item->u.i32.maxval,
+						&val))
+					*(int32_t *)param_addr = (int32_t)val;
+				else
+					errors++;
+				break;
+			case CONFIG_UINT32:
+				if (convert_uint(node, item->u.ui32.minval,
+						 item->u.ui32.maxval,
+						 &uval)) {
+					if (item->flags & CONFIG_MODE)
+						uval = unix2fsal_mode(uval);
+					*(uint32_t *)param_addr = (uint32_t)uval;
+				} else
+					errors++;
+				break;
+			case CONFIG_INT64:
+				if (convert_int(node, item->u.i64.minval,
+						item->u.i64.maxval,
+						&val))
+					*(int64_t *)param_addr = val;
+				else
+					errors++;
+				break;
+			case CONFIG_UINT64:
+				if (convert_uint(node, item->u.ui64.minval,
+						 item->u.ui64.maxval,
+						 &uval))
+					*(uint64_t *)param_addr = uval;
+				else
+					errors++;
+				break;
+			case CONFIG_STRING:
+				if (*(char **)param_addr != NULL)
+					gsh_free(*(char **)param_addr);
+				*(char **)param_addr =
+					gsh_strdup(node->u.varvalue);
+				break;
+			case CONFIG_PATH:
+				if (*(char **)param_addr != NULL)
+					gsh_free(*(char **)param_addr);
+				/** @todo validate path with access() */
+				*(char **)param_addr =
+					gsh_strdup(node->u.varvalue);
+				break;
+			case CONFIG_BOOL:
+				if (convert_bool(node, &bval))
+					*(bool *)param_addr = bval;
+				else
+					errors++;
+				break;
+			case CONFIG_LIST:
+				if (item->u.lst.def == *(uint32_t *)param_addr)
+					*(uint32_t *)param_addr = 0;
+				rc = convert_list(node, item, &flags);
+				if (rc == 0)
+					*(uint32_t *)param_addr |= flags;
+				else
+					errors += rc;
+				break;
+			case CONFIG_ENUM:
+				if (item->u.lst.def == *(uint32_t *)param_addr)
+					*(uint32_t *)param_addr = 0;
+				rc = convert_enum(node, item, &flags);
+				if (rc == 0)
+					*(uint32_t *)param_addr = flags;
+				else
+					errors += rc;
+				break;
+			case CONFIG_IPV4_ADDR:
+				sock = (struct sockaddr *)param_addr;
+
+				rc = convert_inet_addr(node, item,
+						       AF_INET, sock);
+				if (rc != 0)
+					errors++;
+				break;
+			case CONFIG_IPV6_ADDR:
+				sock = (struct sockaddr *)param_addr;
+
+				rc = convert_inet_addr(node, item,
+						       AF_INET6, sock);
+				if (rc != 0)
+					errors++;
+				break;
+			case CONFIG_INET_PORT:
+				if (convert_uint(node, item->u.ui16.minval,
+						 item->u.ui16.maxval,
+						 &uval))
+					*(uint16_t *)param_addr =
+						htons((uint16_t)uval);
+				else
+					errors++;
+				break;
+			case CONFIG_BLOCK:
+				rc = do_subblock(node, item, param_addr);
+				if (rc != 0)
+					errors++;
+				break;
+			default:
+				LogCrit(COMPONENT_CONFIG,
+					"Cannot set value for type(%d) yet",
+					item->type);
+				break;
+			}
+			node = next_node;
+		}
+	}
+	/* We've been marking config nodes as being "seen" during the
+	 * scans.  Report the bogus and typo inflicted bits.
+	 */
+	glist_for_each(ns, &blk->u.sub_nodes) {
+		node = glist_entry(ns, struct config_node, node);
+		if (node->found)
+			node->found = false;
+		else
+			LogMajor(COMPONENT_CONFIG,
+				 "At (%s:%d): Unknown parameter (%s)",
+				 node->filename,
+				 node->linenumber,
+				 node->name);
+	}
+	return errors;
+}
+
+/**
+ * @brief Fill configuration structure from parse tree
+ *
+ */
+
+int load_config_from_parse(config_file_t config,
+			   struct config_block *conf_blk,
+			   void *param,
+			   bool unique)
+{
+	struct config_root *tree = (struct config_root *)config;
+	struct config_node *node;
+	struct glist_head *ns;
+	char *blkname = conf_blk->name;
+	bool found = false;
+	int rc, cum_errs = 0;
+
+	cum_errs = do_block_init(conf_blk->params, param);
+	glist_for_each(ns, &tree->nodes) {
+		node = glist_entry(ns, struct config_node, node);
+		if (node->type == TYPE_BLOCK &&
+		    strcasecmp(blkname, node->name) == 0) {
+			if (found && unique) {
+				LogWarn(COMPONENT_CONFIG,
+					"(%s:%d): Only one %s block allowed",
+					node->filename,
+					node->linenumber,
+					blkname);
+			} else {
+				found = true;
+				rc = do_block_load(node, conf_blk->params, param);
+				if (rc != 0) {
+					LogMajor(COMPONENT_CONFIG,
+						 "Found %d errors in block %s",
+						 rc,
+						 blkname);
+					cum_errs += rc;
+				}
+			}
+		}
+	}
+	if (!found) {
+		LogWarn(COMPONENT_CONFIG,
+			 "Block %s not found. Using defaults", blkname);
+	}
+	if (cum_errs != 0)
+		LogMajor(COMPONENT_CONFIG,
+			 "%d errors found in configuration block %s",
+			 cum_errs, blkname);
+	return cum_errs;
 }
 
 /**
--- src/include/config_parsing.h
+++ src/include/config_parsing.h
@@ -22,6 +22,9 @@
 #define _CONFIG_PARSING_H
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <sys/types.h>
 
@@ -30,6 +33,294 @@ typedef caddr_t config_file_t;
 typedef caddr_t config_item_t;
 
 typedef enum { CONFIG_ITEM_BLOCK = 1, CONFIG_ITEM_VAR } config_item_type;
+
+/**
+ * @brief Data structures for config parse tree processing
+ */
+
+enum config_type {
+	CONFIG_NULL = 0,
+	CONFIG_INT16,
+	CONFIG_UINT16,
+	CONFIG_INT32,
+	CONFIG_UINT32,
+	CONFIG_INT64,
+	CONFIG_UINT64,
+	CONFIG_STRING,
+	CONFIG_PATH,
+	CONFIG_LIST,
+	CONFIG_ENUM,
+	CONFIG_TOKEN,
+	CONFIG_BOOL,
+	CONFIG_IPV4_ADDR,
+	CONFIG_IPV6_ADDR,
+	CONFIG_INET_PORT,
+	CONFIG_BLOCK,
+	CONFIG_PROC
+};
+
+#define CONFIG_UNIQUE		0x001
+#define CONFIG_MANDATORY	0x002
+#define CONFIG_MODE		0x004
+
+struct config_block;
+struct config_item;
+
+/**
+ * @brief token list for CSV options
+ */
+
+struct config_item_list {
+	const char *token;
+	uint32_t value;
+};
+
+#define CONFIG_LIST_TOK(_token_, _flags_) \
+	{ .token = _token_, .value = _flags_}
+
+#define CONFIG_LIST_EOL { .token = NULL, .value = 0}
+/**
+ * @brief A config file parameter
+ *
+ * These are structured as an initialized array with
+ * CONFIG_EOL as the last initializer.
+ */
+
+struct config_item {
+	char *name;
+	enum config_type type;
+	int flags;
+	union {
+		struct { /* CONFIG_BOOL */
+			bool def;
+		} b;
+		struct { /* CONFIG_STRING | CONFIG_PATH */
+			int minsize;
+			int maxsize;
+			const char *def;
+		} str;
+		struct { /* CONFIG_IPV4_ADDR */
+			const char *def;
+		} ipv4;
+		struct { /* CONFIG_IPV6_ADDR */
+			const char *def;
+		} ipv6;
+		struct { /* CONFIG_INT16 */
+			int16_t minval;
+			int16_t maxval;
+			int16_t def;
+		} i16;
+		struct { /* CONFIG_UINT16 */
+			uint16_t minval;
+			uint16_t maxval;
+			uint16_t def;
+		} ui16;
+		struct { /* CONFIG_INT32 */
+			int64_t minval;
+			int64_t maxval;
+			int64_t def;
+		} i32;
+		struct { /* CONFIG_UINT32 */
+			uint32_t minval;
+			uint32_t maxval;
+			uint32_t def;
+		} ui32;
+		struct { /* CONFIG_INT64 */
+			int64_t minval;
+			int64_t maxval;
+			int64_t def;
+		} i64;
+		struct { /* CONFIG_UINT64 */
+			uint64_t minval;
+			uint64_t maxval;
+			uint64_t def;
+		} ui64;
+		struct { /* CONFIG_LIST | CONFIG_ENUM */
+			uint32_t def;
+			struct config_item_list *tokens;
+		} lst;
+		struct { /* CONFIG_BLOCK */
+			void *(*param_mem)(void *parent, void *child);
+			struct config_item *sub_blk;
+			void (*attach)(void *parent, void *child);
+		} blk;
+		struct { /* CONFIG_PROC */
+			struct conf_item_list *tokens;
+			uint32_t def;
+			void (*setf)(void *);
+		} proc;
+	} u;
+	size_t off; /* offset into struct pointed to by opaque_dest */
+};
+
+#define CONF_ITEM_BLOCK(_name_, _mem_, _params_, _attach_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_PROC,		    \
+	  .u.blk.param_mem = _mem_,			    \
+	  .u.blk.sub_blk = _params_,		    \
+	  .u.blk.attach = _attach_,	    \
+	  .off = 0L				\
+	}
+
+#define CONF_ITEM_PROC(_name_, _def_, _tokens_, _proc_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_PROC,		    \
+	  .u.proc.def = _def_,			    \
+	  .u.proc.tokens = _tokens_,		    \
+	  .u.proc.setf = _proc_	    \
+	}
+
+#define CONF_ITEM_LIST(_name_, _def_, _tokens_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_LIST,		    \
+	  .u.lst.def = _def_,			    \
+	  .u.lst.tokens = _tokens_,		    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_ENUM(_name_, _def_, _tokens_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_ENUM,		    \
+	  .u.lst.def = _def_,			    \
+	  .u.lst.tokens = _tokens_,		    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_UNIQ_ENUM(_name_, _def_, _tokens_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_ENUM,		    \
+	  .flags = CONFIG_UNIQUE,		    \
+	  .u.lst.def = _def_,			    \
+	  .u.lst.tokens = _tokens_,		    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_BOOL(_name_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_BOOL,		    \
+	  .u.b.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_STR(_name_, _minsize_, _maxsize_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_STRING,		    \
+	  .u.str.minsize = _minsize_,		    \
+	  .u.str.maxsize = _maxsize_,		    \
+	  .u.str.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+#define CONF_ITEM_PATH(_name_, _minsize_, _maxsize_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_PATH,		    \
+	  .u.str.minsize = _minsize_,		    \
+	  .u.str.maxsize = _maxsize_,		    \
+	  .u.str.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+#define CONF_ITEM_IPV4_ADDR(_name_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_IPV4_ADDR,		    \
+	  .u.ipv4.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_IPV6_ADDR(_name_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_IPV6_ADDR,		    \
+	  .u.ipv6.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_INET_PORT(_name_, _min_, _max_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_INET_PORT,		    \
+	  .u.i16.minval = _min_,		    \
+	  .u.i16.maxval = _max_,		    \
+	  .u.i16.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_UI16(_name_, _min_, _max_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_UINT16,		    \
+	  .u.ui16.minval = _min_,		    \
+	  .u.ui16.maxval = _max_,		    \
+	  .u.ui16.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_UI16(_name_, _min_, _max_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_UINT16,		    \
+	  .u.ui16.minval = _min_,		    \
+	  .u.ui16.maxval = _max_,		    \
+	  .u.ui16.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_I32(_name_, _min_, _max_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_INT32,		    \
+	  .u.i32.minval = _min_,		    \
+	  .u.i32.maxval = _max_,		    \
+	  .u.i32.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_UI32(_name_, _min_, _max_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_UINT32,		    \
+	  .u.ui32.minval = _min_,		    \
+	  .u.ui32.maxval = _max_,		    \
+	  .u.ui32.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_MODE(_name_, _min_, _max_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_UINT32,		    \
+	  .flags = CONFIG_MODE,			    \
+	  .u.ui32.minval = _min_,		    \
+	  .u.ui32.maxval = _max_,		    \
+	  .u.ui32.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_I64(_name_, _min_, _max_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_INT64,		    \
+	  .u.i64.minval = _min_,		    \
+	  .u.i64.maxval = _max_,		    \
+	  .u.i64.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONF_ITEM_UI64(_name_, _min_, _max_, _def_, _struct_, _mem_) \
+	{ .name = _name_,			    \
+	  .type = CONFIG_UINT64,		    \
+	  .u.ui64.minval = _min_,		    \
+	  .u.ui64.maxval = _max_,		    \
+	  .u.ui64.def = _def_,			    \
+	  .off = offsetof(struct _struct_, _mem_)   \
+	}
+
+#define CONFIG_EOL {.name = NULL, .type = CONFIG_NULL}
+
+/**
+ * @brief Configuration Block
+ *
+ * This is used for both config file parse tree processing
+ * and DBus property settings.
+ */
+
+struct config_block {
+	char *name;
+	char *dbus_interface_name;
+	struct config_item *params;
+};
+	
+	
 
 /* config_ParseFile:
  * Reads the content of a configuration file and
@@ -53,6 +344,14 @@ void config_Print(FILE *output, config_file_t config);
 
 /* Free the memory structure that store the configuration. */
 void config_Free(config_file_t config);
+
+int load_config_from_parse(config_file_t config,
+			   struct config_block *conf_blk,
+			   void *param,
+			   bool unique);
+/*
+ * Old, deprecated API.  Will disappear
+ */
 
 /* Indicates how many main blocks are defined into the config file.
  * \return A positive value if no error.
