55ba561b1c1b6a1276845405c626bd32d194da7d
Matt Benjamin <mbenjamin@redhat.com>
2015-08-02 13:57:26 -0400

    testing: improve nfs_lib and ci_hash
    REBASEME
    
    Change-Id: I5069268f4e8f6b77ebf75ee06fea546c44e3b2be
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/gtest/test_ci_hash_dist1.cc
+++ src/gtest/test_ci_hash_dist1.cc
@@ -26,50 +26,33 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <thread>
 #include <random>
 #include "gtest/gtest.h"
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/exception.hpp>
 
 extern "C" {
-  /* Ganesha headers */
+/* Ganesha headers */
+#include "nfs_lib.h"
 }
 
 namespace bf = boost::filesystem;
 
 namespace {
 
-  bool create = false; // will pre-destroy
-  bool destroy = false;
-
 #if 0
-  vnode_t* root_vnode = nullptr;
-  inogen_t root_ino = {0, 0};
-  creden_t acred = {0, 0};
-
-  struct ZFSObject
-  {
-    std::string leaf_name;
-    inogen_t ino;
-    vnode_t* vnode;
-
-  ZFSObject(std::string n) : leaf_name(std::move(n)), ino{0, 0},
-      vnode(nullptr)
-    {}
-  };
-
-  std::vector<ZFSObject> zfs1_objs;
-  std::vector<ZFSObject> zfs2_objs;
-
-  static constexpr int n_objs_c2 = 10000;
-
   std::uniform_int_distribution<uint8_t> uint_dist;
   std::mt19937 rng;
 
   p->cksum = XXH64(p->data, 65536, 8675309);
-
-  ZFSObject c2_o(std::string("creates2"));
 #endif
+
+  int ganesha_server() {
+    /* XXX */
+    return nfs_libmain();
+  }
+
 } /* namespace */
 
 TEST(CI_HASH_DIST1, INIT)
@@ -77,81 +60,13 @@ TEST(CI_HASH_DIST1, INIT)
   ASSERT_EQ(0, 0);
 }
 
-
-#if 0
-TEST(ZFSIO, INIT)
-{
-  zhd = lzfw_init();
-  ASSERT_NE(zhd, nullptr);
-
-  ASSERT_EQ(is_directory(vdevs), true);  
-  vdev1 /= "zd2";
-}
-
-TEST(ZFSIO, CREATEF1)
-{
-  int err, ix;
-  zfs1_objs.reserve(100);
-  for (ix = 0; ix < 100; ++ix) {
-    unsigned o_flags;
-    std::string n{"f" + std::to_string(ix)};
-    zfs1_objs.emplace_back(ZFSObject(n));
-    ZFSObject& o = zfs1_objs[ix];
-    /* create and open */
-    err = lzfw_openat(zhfs, &acred, root_vnode, o.leaf_name.c_str(),
-		      O_RDWR|O_CREAT, 644, &o_flags, &o.vnode);
-    ASSERT_EQ(err, 0);
-  }
-}
-
-TEST(ZFSIO, CLOSE1)
-{
-  int err, ix;
-  for (ix = 0; ix < 100; ++ix) {
-    ZFSObject& o = zfs1_objs[ix];
-    err = lzfw_close(zhfs, &acred, o.vnode, O_RDWR|O_CREAT);
-    ASSERT_EQ(err, 0);
-  }
-}
-
-TEST(ZFSIO, SHUTDOWN)
-{
-  int err;
-  const char* lzw_err;
-
-  // close root vnode
-  err = lzfw_closedir(zhfs, &acred, root_vnode);
-  root_vnode = nullptr;
-  ASSERT_EQ(err, 0);
-
-  // release fs
-  err = lzfw_umount(zhfs, true /* force */);
-  zhfs = nullptr;
-  ASSERT_EQ(err, 0);
-
-  // cond destroy everything
-  if (destroy) {
-    { // destroy fs
-      char* fs = strdup("zp2/zf2");
-      err = lzfw_dataset_destroy(zhd, fs, &lzw_err);
-      ASSERT_EQ(err, 0);
-      free(fs);
-    }
-    { // destroy pool
-      err = lzfw_zpool_destroy(zhd, "zp2", true /* force */, &lzw_err);
-      ASSERT_EQ(err, 0);
-      remove(vdev1);
-    }
-  }
-
-  // release library
-  lzfw_exit(zhd);
-  zhd = nullptr;
-}
-#endif
-
 int main(int argc, char *argv[])
 {
+  int code;
   ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  std::thread ganesha(ganesha_server);
+  code  = RUN_ALL_TESTS();
+  ganesha.join();
+
+  return code;
 }
