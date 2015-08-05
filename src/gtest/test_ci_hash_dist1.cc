c1d6c444203c6f963e6d1887fe93a244b603d673
Matt Benjamin <mbenjamin@redhat.com>
2015-08-05 10:31:14 -0400

    ci_test_hash_dist:  add creates, deletes in test_root
    Verified for a VFS export.
    
    Change-Id: I516f3fce1c670d13cc7b3508d9f2b7ffb88f93cd
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/gtest/test_ci_hash_dist1.cc
+++ src/gtest/test_ci_hash_dist1.cc
@@ -39,10 +39,15 @@ extern "C" {
 #include "nfs_lib.h"
 #include "export_mgr.h"
 #include "cache_inode.h"
+#include "cache_inode_lru.h"
 #include "nfs_exports.h"
 #include "fsal.h"
 }
 
+using namespace std;
+using namespace std::literals;
+
+namespace po = boost::program_options;
 namespace bf = boost::filesystem;
 
 namespace {
@@ -52,12 +57,24 @@ namespace {
   int dlevel = -1;
   uint16_t export_id = 77;
 
+  uint32_t n_objects = 250*1000;
+
   struct req_op_context req_ctx;
   struct user_cred user_credentials;
 
   struct gsh_export* a_export = nullptr;
   cache_entry_t* root_entry = nullptr;
   cache_entry_t* test_root = nullptr;
+
+  struct TFile {
+    std::string leaf_name;
+    cache_entry_t* entry;
+
+  TFile(std::string n) : leaf_name(std::move(n)), entry(nullptr)
+      {}
+  };
+
+  std::vector<TFile> test_objs;
 
 #if 0
   std::uniform_int_distribution<uint8_t> uint_dist;
@@ -85,6 +102,7 @@ TEST(CI_HASH_DIST1, INIT)
   ASSERT_NE(a_export, nullptr);
 
   status = nfs_export_get_root_entry(a_export, &root_entry);
+  ASSERT_EQ(status, CACHE_INODE_SUCCESS);
   ASSERT_NE(root_entry, nullptr);
 
   /* Ganesha call paths need real or forged context info */
@@ -107,16 +125,60 @@ TEST(CI_HASH_DIST1, CREATE_ROOT)
   status = cache_inode_create(root_entry, "ci_hash_dist1",
 			      DIRECTORY, 777, NULL /* create arg */,
 			      &test_root);
+  ASSERT_EQ(status, CACHE_INODE_SUCCESS);
   ASSERT_NE(test_root, nullptr);
+}
+
+TEST(CI_HASH_DIST1, CREATEF1)
+{
+  cache_inode_status_t status;
+  test_objs.reserve(n_objects);
+  for (int ix = 0; ix < n_objects; ++ix) {
+    string n{"f" + std::to_string(ix)};
+    test_objs.emplace_back(TFile(n));
+    TFile& o = test_objs[ix];
+    status = cache_inode_create(test_root, o.leaf_name.c_str(),
+				REGULAR_FILE, 644, NULL /* create arg */,
+				&o.entry);
+    ASSERT_EQ(status, CACHE_INODE_SUCCESS);
+    ASSERT_NE(o.entry, nullptr);
+  }
+}
+
+TEST(CI_HASH_DIST, WAIT1)
+{
+  cout << "Thread in WAIT1" << endl;
+  std::this_thread::sleep_for(5s);
+}
+
+/* XXX cache_inode_get stress and bench */
+/* XXX check dist */
+
+TEST(CI_HASH_DIST1, REMOVE1)
+{
+  cache_inode_status_t status;
+  for (int ix = 0; ix < n_objects; ++ix) {
+    TFile& o = test_objs[ix];
+    status = cache_inode_remove(test_root, o.leaf_name.c_str());
+    ASSERT_EQ(status, CACHE_INODE_SUCCESS);
+    /* put last ref */
+    cache_inode_put(o.entry); /* XXX no status */
+  }
+}
+
+TEST(CI_HASH_DIST1, REMOVE_ROOT)
+{
+  cache_inode_status_t status;
+  status = cache_inode_remove(root_entry, "ci_hash_dist1");
+  ASSERT_EQ(status, CACHE_INODE_SUCCESS);
+  /* put last ref */
+  cache_inode_put(test_root); /* XXX no status */
+  test_root = nullptr;
 }
 
 int main(int argc, char *argv[])
 {
   int code = 0;
-
-  using namespace std;
-  using namespace std::literals;
-  namespace po = boost::program_options;
 
   po::options_description opts("program options");
   po::variables_map vm;
@@ -132,6 +194,9 @@ int main(int argc, char *argv[])
 
       ("export", po::value<uint16_t>(),
 	"id of export on which to operate (must exist)")
+
+      ("nobjects", po::value<uint32_t>(),
+	"count of file objects to create (single dir, default 250K)")
 
       ("debug", po::value<string>(),
 	"ganesha debug level")
@@ -150,20 +215,26 @@ int main(int argc, char *argv[])
     if (vm_iter != vm.end()) {
       lpath = (char*) vm_iter->second.as<std::string>().c_str();
     }
+    vm_iter = vm.find("export");
+    if (vm_iter != vm.end()) {
+      export_id = vm_iter->second.as<uint16_t>();
+    }
+    vm_iter = vm.find("nobjects");
+    if (vm_iter != vm.end()) {
+      n_objects = vm_iter->second.as<uint32_t>();
+    }
     vm_iter = vm.find("debug");
     if (vm_iter != vm.end()) {
       dlevel = ReturnLevelAscii(
 	(char*) vm_iter->second.as<std::string>().c_str());
     }
-    vm_iter = vm.find("export");
-    if (vm_iter != vm.end()) {
-      export_id = vm_iter->second.as<uint16_t>();
-    }
 
     ::testing::InitGoogleTest(&argc, argv);
 
     std::thread ganesha(ganesha_server);
+    cout << "In WAIT for ganesha startup" << endl;
     std::this_thread::sleep_for(5s);
+    cout << "Start" << endl;
 
     code  = RUN_ALL_TESTS();
     ganesha.join();
