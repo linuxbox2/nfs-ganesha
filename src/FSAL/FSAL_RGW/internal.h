d238d706c0bb42a6ba7f96918b39b536676d1346
Matt Benjamin <mbenjamin@redhat.com>
2015-09-02 19:00:06 -0400

    tyop
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/FSAL/FSAL_RGW/internal.h
+++ src/FSAL/FSAL_RGW/internal.h
@@ -66,7 +66,7 @@ extern struct rgw_fsal_module RGWFSM;
 struct rgw_export {
 	struct fsal_export export;	/*< The public export object */
 	struct rgw_handle *root;	/*< The root handle */
-	char rgw_name[NAME_MAX + 1]
+	char rgw_name[NAME_MAX + 1];
 	char rgw_user_id[MAXUIDLEN + 1];
 	char rgw_access_key_id[MAXKEYLEN + 1];
 	char rgw_secret_access_key[MAXSECRETLEN + 1];
