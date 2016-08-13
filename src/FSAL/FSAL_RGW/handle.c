c4822416a6051687feab5bff7854a00f91b641da
Matt Benjamin <mbenjamin@redhat.com>
2016-08-13 17:21:02 -0400

    RGW: try-implement rgw_fsal_close
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/FSAL/FSAL_RGW/handle.c
+++ src/FSAL/FSAL_RGW/handle.c
@@ -1642,6 +1642,63 @@ static fsal_status_t rgw_fsal_close(struct fsal_obj_handle *obj_hdl)
 }
 
 /**
+ * @brief Manage closing a file when a state is no longer needed.
+ *
+ * When the upper layers are ready to dispense with a state, this method is
+ * called to allow the FSAL to close any file descriptors or release any other
+ * resources associated with the state. A call to free_state should be assumed
+ * to follow soon.
+ *
+ * @param[in] obj_hdl    File on which to operate
+ * @param[in] state      state_t to use for this operation
+ *
+ * @return FSAL status.
+ */
+
+fsal_status_t rgw_fsal_close2(struct fsal_obj_handle *obj_hdl,
+			struct state_t *state)
+{
+	int rc;
+	struct rgw_open_state *open_state;
+
+	struct rgw_export *export =
+	    container_of(op_ctx->fsal_export, struct rgw_export, export);
+
+	struct rgw_handle *handle = container_of(obj_hdl, struct rgw_handle,
+						 handle);
+	
+	if (state) {
+		open_state = (struct rgw_open_state *) state;
+
+		LogFullDebug(COMPONENT_FSAL,
+			"%s called w/open_state %p", __func__, open_state);
+		
+		if (state->state_type == STATE_TYPE_SHARE ||
+			state->state_type == STATE_TYPE_NLM_SHARE ||
+			state->state_type == STATE_TYPE_9P_FID) {
+			/* This is a share state, we must update the share
+			 * counters.  This can block over an I/O operation.
+			 */
+			PTHREAD_RWLOCK_wrlock(&obj_hdl->lock);
+
+			update_share_counters(&handle->share,
+					handle->openflags,
+					FSAL_O_CLOSED);
+
+			PTHREAD_RWLOCK_unlock(&obj_hdl->lock);
+		}
+	}
+
+	rc = rgw_close(export->rgw_fs, handle->rgw_fh, RGW_CLOSE_FLAG_NONE);
+	if (rc < 0)
+		return rgw2fsal_error(rc);
+
+	handle->openflags = FSAL_O_CLOSED;
+
+	return fsalstat(ERR_FSAL_NO_ERROR, 0);
+}
+
+/**
  * @brief Write wire handle
  *
  * This function writes a 'wire' handle to be sent to clients and
