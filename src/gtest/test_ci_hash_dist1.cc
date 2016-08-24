306a75770926169d9cb54ced55f56cb9f4377045
Matt Benjamin <mbenjamin@redhat.com>
2016-08-24 13:40:59 -0400

    gtest: improve ci_hash
    Change-Id: I69d65eef3ab3dd194040e939a2926a97d438532e
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/gtest/test_ci_hash_dist1.cc
+++ src/gtest/test_ci_hash_dist1.cc
@@ -1,1 +1,72 @@
-// placeholder
+// -*- mode:C; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Copyright (C) 2015 Red Hat, Inc.
+ * Contributor : Matt Benjamin <mbenjamin@redhat.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ * -------------
+ */
+
+#include <sys/types.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <thread>
+#include <random>
+#include "gtest/gtest.h"
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/exception.hpp>
+
+extern "C" {
+/* Ganesha headers */
+#include "nfs_lib.h"
+}
+
+namespace bf = boost::filesystem;
+
+namespace {
+
+#if 0
+  std::uniform_int_distribution<uint8_t> uint_dist;
+  std::mt19937 rng;
+
+  p->cksum = XXH64(p->data, 65536, 8675309);
+#endif
+
+  int ganesha_server() {
+    /* XXX */
+    return nfs_libmain();
+  }
+
+} /* namespace */
+
+TEST(CI_HASH_DIST1, INIT)
+{
+  ASSERT_EQ(0, 0);
+}
+
+int main(int argc, char *argv[])
+{
+  int code;
+  ::testing::InitGoogleTest(&argc, argv);
+  std::thread ganesha(ganesha_server);
+  code  = RUN_ALL_TESTS();
+  ganesha.join();
+
+  return code;
+}
